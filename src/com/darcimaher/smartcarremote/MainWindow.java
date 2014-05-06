a5f808eb4b4d8c4a37b30c587edfc83902ed3167
Toby Darci-Maher <toby.darcimaher@gmail.com>
2014-05-05 22:10:11 -0700

    Red Boxes

--- src/com/darcimaher/smartcarremote/MainWindow.java
+++ src/com/darcimaher/smartcarremote/MainWindow.java
@@ -3,126 +3,144 @@ package com.darcimaher.smartcarremote;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.LineBorder;
 
 public class MainWindow {
 
-	private JFrame frame;
-	private JPanel textPanel;
-	private JTextArea console;
-	private JTextArea keyArea;
-	private JScrollPane pScroll;
+    public MainWindow() {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException  ex) {
+                }
+                catch ( InstantiationException ex) {
+                }
+                catch (IllegalAccessException  ex) {
+                }
+                catch ( UnsupportedLookAndFeelException ex) {
+                }
 
-	public MainWindow() {
-		this.createWindow();
-		this.connectToCar();
-	}
+                JFrame frame = new JFrame("Smart Car Remote");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
+                frame.setLayout(new BorderLayout());
+                frame.add(new KeyPad());
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    }
 
-	private void createWindow() {
+    public class KeyPad extends JPanel {
 
-		System.out.println("Creating window...");
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
 
-		// make the window itself
-		frame = new JFrame("Car Remote");
-		frame.setSize(624, 350);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		public KeyPad() {
 
-		// make a panel to hold console and key area
-		textPanel = new JPanel(new GridLayout(1, 2));
+            setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.gridx = 1;
+            gbc.gridy = 0;
+            SingleKeyDelegate myDelegate = new SingleKeyDelegate() {
+				
+				@Override
+				public void keyTouched() {
+					System.out.println("SOMEBODY TOUCHED A BUTTON");
+				}
+			};
 
-		// make console
-		console = new JTextArea();
-		console.setBackground(Color.CYAN);
-		pScroll = new JScrollPane(console,
-				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		pScroll.setPreferredSize(new Dimension(500, 250));
-		textPanel.add(pScroll);
+            add(new SingleKey(KeyEvent.VK_UP, 0, myDelegate), gbc);
 
-		// make area for listening to keys
-		keyArea = new JTextArea();
-		keyArea.setBackground(Color.YELLOW);
-		keyArea.setPreferredSize(new Dimension(100, 100));
-		keyArea.addKeyListener(new MainWindowKeyListener());// add key listener
-															// to listen
-		// to key events such as
-		// press, etc
-		textPanel.add(keyArea);
+            gbc.gridy = 2;
+            add(new SingleKey(KeyEvent.VK_DOWN, 0, myDelegate), gbc);
 
-		// add textPanel and buttonPanel to the window
-		frame.getContentPane().add(textPanel, BorderLayout.CENTER);
-		textPanel.revalidate();
-	}
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            add(new SingleKey(KeyEvent.VK_LEFT, 0, myDelegate), gbc);
 
-	private void connectToCar() {
-		System.out.println("Connecting to car...");
-	}
+            gbc.gridx = 2;
+            add(new SingleKey(KeyEvent.VK_RIGHT, 0, myDelegate), gbc);
 
-	class MainWindowKeyListener implements KeyListener {
+        }
+    }
 
-		@Override
-		public void keyPressed(KeyEvent e) {
-			try {
-				if (e.getKeyCode() == 37)// left arrow keycode, tell NXT to
-											// turn right
-				{
-					console.append("Left command sent.\n");
-//					dos.writeInt(e.getKeyCode());
-//					dos.flush();
-				} else if (e.getKeyCode() == 38)// up arrow keycode, tell NXT to
-												// go forward
-				{
-					console.append("Forward command sent.\n");
-//					dos.writeInt(e.getKeyCode());
-//					dos.flush();
-				} else if (e.getKeyCode() == 39)// left arrow keycode, tell NXT
-												// to turn left
-				{
-					console.append("Right command sent.\n");
-//					dos.writeInt(e.getKeyCode());
-//					dos.flush();
-				} else if (e.getKeyCode() == 40)// down arrow keycode, tell NXT
-												// to go backwards
-				{
-					console.append("Backwards command sent.\n");
-//					dos.writeInt(e.getKeyCode());
-//					dos.flush();
-				} else {
-					console.append("Unrecognized command.\n");
-				}// some other key pressed, not any of the four arrow keys, warn
-					// that the command is unrecognized
-			}
+    public interface SingleKeyDelegate {
+    	public void keyTouched();
+    }
+    
+    public class SingleKey extends JPanel {
 
-			catch (Exception E) {
-				console.append("Could not send command\n");
-			}// catch exception, save face
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private final SingleKeyDelegate keyDelegate;
 
-		}
+		public SingleKey(int keyCode, int modifier, SingleKeyDelegate thingie) {
 
-		@Override
-		public void keyReleased(KeyEvent e) {
-			try {
-//				dos.writeInt(-2);// NXT recognises -2 as command to stop
-//									// whatever it is doing whether it is
-//									// turning left, right or going forwards or
-//									// backwards.
-//				dos.flush();
-			} catch (Exception E) {
-			}
-		}
+			this.keyDelegate = thingie;
+            setBorder(new LineBorder(Color.BLACK));
 
-		@Override
-		public void keyTyped(KeyEvent e) {
-		}
+            InputMap im = getInputMap(WHEN_IN_FOCUSED_WINDOW);
+            ActionMap am = getActionMap();
 
-	}
+            im.put(KeyStroke.getKeyStroke(keyCode, modifier, false), "keyPressed");
+            im.put(KeyStroke.getKeyStroke(keyCode, modifier, true), "keyReleased");
 
+            am.put("keyPressed", new AbstractAction() {
+                /**
+				 * 
+				 */
+				private static final long serialVersionUID = 1L;
+
+				@Override
+                public void actionPerformed(ActionEvent e) {
+                    setBackground(Color.RED);
+                    // tell our parent that this happened
+                    keyDelegate.keyTouched();
+                }
+            });
+
+            am.put("keyReleased", new AbstractAction() {
+                /**
+				 * 
+				 */
+				private static final long serialVersionUID = 1L;
+
+				@Override
+                public void actionPerformed(ActionEvent e) {
+                    setBackground(UIManager.getColor("Panel.background"));
+                    // tell our parent that this happened
+                    keyDelegate.keyTouched();
+                }
+            });
+
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(100, 100);
+        }
+    }
+    
 }
--- src/com/darcimaher/smartcarremote/StartHereMan.java
+++ src/com/darcimaher/smartcarremote/StartHereMan.java
@@ -3,7 +3,7 @@ package com.darcimaher.smartcarremote;
 public class StartHereMan {
 
 	public static void main(String[] args) {
-		MainWindow myWindow = new MainWindow();
+		new MainWindow();
 	}
 
 }
